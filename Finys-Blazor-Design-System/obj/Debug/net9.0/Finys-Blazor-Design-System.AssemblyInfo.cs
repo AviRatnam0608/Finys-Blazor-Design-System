fff3451645a61509a78a05290ec5cdcade05359e
Avinash <avinashr@umich.edu>
2025-05-12 17:47:54 -0400

    Added test blazor code

--- Finys-Blazor-Design-System/obj/Debug/net9.0/Finys-Blazor-Design-System.AssemblyInfo.cs
+++ Finys-Blazor-Design-System/obj/Debug/net9.0/Finys-Blazor-Design-System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finys-Blazor-Design-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a44e3f0cce4368534336d7cf61cbdf70c5f4b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138c4d402ccae6af48cc81ae11577ea8283b971d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finys-Blazor-Design-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finys-Blazor-Design-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
