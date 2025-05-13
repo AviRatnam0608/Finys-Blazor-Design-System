96d802eae677eddc7e62c03ba1adeb9c883533f8
Bustamante, Nathan <NBustamante@finys.com>
2025-05-12 21:41:32 -0400

    testing TelerikGrid component

---
+++ Finys-Blazor-Design-System/Models/GridData.cs
@@ -0,0 +1,10 @@
+﻿namespace Finys_Blazor_Design_System.Models;
+public class GridData
+{
+    public int Id { get; set; }
+    public string? Claimant { get; set; }
+    public string? Adjuster { get; set; }
+    public string? Coverage { get; set; }
+    public string? Cause { get; set; }
+}
+
